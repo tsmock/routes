5613cd779e27d62c5b9cd8d5dadc7cae5fb26222
Jiri Klement <jiri.klement@gmail.com>
2010-03-11 20:26:09 +0000

    OsmPrimitive.highlighted => OsmPrimitive.setHighlighted()

--- src/org/openstreetmap/josm/plugins/routes/RelationEditMode.java
+++ src/org/openstreetmap/josm/plugins/routes/RelationEditMode.java
@@ -21,56 +21,56 @@ import org.openstreetmap.josm.tools.Shortcut;
 
 public class RelationEditMode extends MapMode {
     private static final long serialVersionUID = -7767329767438266289L;
-    
+
     private Way highlightedWay;
-    
+
     public RelationEditMode(MapFrame mapFrame) {
         super(tr("Edit relation"), "node/autonode", tr("Edit relations"),
                 Shortcut.registerShortcut("mapmode:editRelation", tr("Mode: {0}", tr("Edit relation")), KeyEvent.VK_H, Shortcut.GROUP_EDIT),
                 mapFrame, Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR));
     }
-    
+
     @Override
     public void enterMode() {
         super.enterMode();
         Main.map.mapView.addMouseListener(this);
         Main.map.mapView.addMouseMotionListener(this);
     }
-    
+
     @Override
     public void exitMode() {
         super.exitMode();
         Main.map.mapView.removeMouseListener(this);
         Main.map.mapView.removeMouseMotionListener(this);
     }
-    
+
     @Override
     public void mouseMoved(MouseEvent e) {
         Way nearestWay = Main.map.mapView.getNearestWay(e.getPoint());
         if (nearestWay != highlightedWay) {
             if (highlightedWay != null) {
-                highlightedWay.highlighted = false;
+                highlightedWay.setHighlighted(false);
             }
             if (nearestWay != null) {
-                nearestWay.highlighted = true;
+                nearestWay.setHighlighted(true);
             }
             highlightedWay = nearestWay;
-            Main.map.mapView.repaint();	
+            Main.map.mapView.repaint();
         }
     }
-    
+
     @Override
     public void mouseClicked(MouseEvent e) {
         Way way = Main.map.mapView.getNearestWay(e.getPoint());
-        
+
         Collection<Relation> selectedRelations = Main.main.getCurrentDataSet().getSelectedRelations();
-        
+
         if (way != null) {
-            
+
             if (selectedRelations.isEmpty()) {
                 JOptionPane.showMessageDialog(Main.parent, tr("No relation is selected"));
             }
-            
+
             for (OsmPrimitive rel:selectedRelations) {
                 Relation r = (Relation)rel;
                 RelationMember foundMember = null;
@@ -80,7 +80,7 @@ public class RelationEditMode extends MapMode {
                         break;
                     }
                 }
-                
+
                 if (foundMember != null) {
                     Main.main.undoRedo.add(new RemoveRelationMemberCommand(r, new RelationMember("", way)));
                 } else {
@@ -92,6 +92,6 @@ public class RelationEditMode extends MapMode {
         }
     }
 
-    
+
 
 }
